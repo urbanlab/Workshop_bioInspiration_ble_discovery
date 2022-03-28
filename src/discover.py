78847d8ce17b4c3bef0133a36c172e9794ee7e95
RoBSPRR <102524697+RoBSPRR@users.noreply.github.com>
2022-03-28 17:15:27 +0200

    Correct Json formatting + too many comments

--- src/discover.py
+++ src/discover.py
@@ -1,13 +1,23 @@
 from bluepy.btle import Scanner
 
+#Declare global variables and create files
 ble_devices = {}
 ble_device = {}
+deviceId = 0
+scanner = Scanner()
+jsonDevices = "devices.json"
 
+#Create a Json with all the devices during one scan (erases the previous Json)
 while True : 
-    f = open("devices.json", "a")
-    scanner = Scanner()
+
+    #Declare loop variables and begin scan
+    f = open(jsonDevices, "a")
     devices = scanner.scan(3.0)
+    deviceId = 0
+
+    # Write the first line of the Json
     f.write("[\n")
+
     for device in devices:
         #print("DEV = {} RSSI = {}".format(device.addr, device.rssi))
         #print(device.getScanData())
@@ -17,14 +27,25 @@ while True :
 
         # Get device infos plus name if ther is one
         #print(str(device.addr) + " " + str(device.rssi))
-        ble_device = str({"adress": device.addr, "signal_strengh": device.rssi }).replace("'", '"')
-        print(ble_device)
-        f.write(ble_device)
-        f.write(",\n")
-    f.write("]")
 
+        #Count the number of iterations
+        deviceId = deviceId + 1
+
+        #Write each the Json line and replace ' with " for correct formatting
+        ble_device = str({"adress": device.addr, "signal_strengh": device.rssi }).replace("'", '"')
+        f.write(ble_device)
+
+        #Add the correct fomating for each line ending (different for te last line)
+        if deviceId == len(devices) :
+            f.write("\n]")
+        else :
+            f.write(",\n")
+        
+        #Print to demonstrate you're a hacker
+        print(ble_device)
+
+    #Erases the content of the Json
     print("#######################################################")
-    with open("devices.json",'w') as f: 
+    with open(jsonDevices,'w') as f: 
         pass
     f.close()
-
