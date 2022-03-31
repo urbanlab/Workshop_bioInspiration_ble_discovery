3295031baa521e980fc0b3971f5e80f768962ed2
Yassin Siouda <yassin@siouda.com>
2022-03-31 10:53:58 +0100

    Device sorting by signal

--- src/discover.py
+++ src/discover.py
@@ -5,54 +5,65 @@ ble_devices = {}
 ble_device = {}
 deviceId = 0
 scanner = Scanner()
+devicesToSort = []
 jsonDevices = "static/devices.json"
 
 #Create a Json with all the devices during one scan (erases the previous Json)
 while True : 
 
     #Declare loop variables and begin scan
-    f = open(jsonDevices, "a")
+    
     devices = scanner.scan(3.0)
     deviceId = 0
 
-    # Write the first line of the Json
-    f.write("[\n")
-
+    #For each device found, write the device in the Json and filter the first one by the signal strenght
     for device in devices:
-        #print("DEV = {} RSSI = {}".format(device.addr, device.rssi))
-        #print(device.getScanData())
-
-        # Get device infos plus name if ther is one
-        #print(str(device.addr) + " " + str(device.rssi) + " " + str(device.getScanData()))
-
-        # Get device infos plus name if ther is one
-        #print(str(device.addr) + " " + str(device.rssi))
 
         #Count the number of iterations
         deviceId = deviceId + 1
 
         #Write each the Json line and replace ' with " for correct formatting
-        ble_device = str({"adress": device.addr, "signal_strengh": device.rssi }).replace("'", '"')
+        ble_device = {"adress": device.addr, "signal_strengh": device.rssi }
         #print(device.getScanData())
-        moreData = device.getScanData()
-        getDescription(adtype)
-        try :
-            print(moreData)
-            #print(moreData[2])
-            
-        except :
-            print("null")
-        f.write(ble_device)
-
-        #Add the correct fomating for each line ending (different for te last line)
-        if deviceId == len(devices) :
-            f.write("\n]")
-        else :
-            f.write(",\n")
+        
+        # push the devices dict in an array to be able to sort them by signal strenght
+        devicesToSort.append(ble_device)
         
         #Print to demonstrate you're a hacker
         print(ble_device)
 
+
+
+    #Sort the devices by signal strenght
+    devicesToSort.sort(key=lambda x: x['signal_strengh'], reverse=True)
+
+    print(devicesToSort)
+    with open(jsonDevices,'w') as f: 
+            # replace single quotes with double quotes for correct formatting
+            f.write(str(devicesToSort).replace("'", '"'))
+            # Write each device in the Json
+            #f.write(str(devicesToSort))
+    f.close()
+
+
+    exit()
+
+    #Print the devices sorted by signal strenght
+    for device in devicesToSort:
+        with open(jsonDevices,'w') as f: 
+            # Write each device in the Json
+            f.write(str(device) + ",\n")
+            # If the device is not the last one, write a comma
+            if deviceId != len(devicesToSort):
+                f.write(",")
+                # Write a new line
+                f.write("\n")
+            else :
+                f.write("]")
+            f.close()
+        
+
+        
     #Erases the content of the Json
     print("#######################################################")
     with open(jsonDevices,'w') as f: 
